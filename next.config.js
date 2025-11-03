902ac714713c6de620c98c9cf0ff102f58f01ed7
themutherfvcker <162934982+themutherfvcker@users.noreply.github.com>
2025-11-03 12:01:26 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
-// next.config.js
-// This configuration enables MDX support in Next.js.
-
+/** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
 })
 
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  images: {
-    // Update this array to include any external image domains you plan to use.
-    domains: ['your-image-cdn.com'],
-  },
 }
 
 module.exports = withMDX(nextConfig)
