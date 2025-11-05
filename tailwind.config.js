242e84c46bf765cc2f929f8ef9c33efe7ac70eb6
themutherfvcker <162934982+themutherfvcker@users.noreply.github.com>
2025-11-06 10:32:59 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./content/**/*.{mdx}"
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./content/**/*.mdx"
+],
+
   ],
   theme: {
     extend: {
